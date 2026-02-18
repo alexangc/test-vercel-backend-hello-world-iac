b0ed02d000ce72488befb80d72c398ebdd2965ba
alexdragongc <alex.miranda@dragongc.com>
2026-02-18 17:47:16 +0100

    Touch to trigger deploy

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.get("/", (_req, res) => {
   res.send("Hello, World! This is a basic Express app deployed on Vercel.");
 });
 
-app.get("/api/hello", (req, res) => {
+app.get("/api/hello", (_, res) => {
   res.json({ message: "Hello from the API!" });
 });
 
